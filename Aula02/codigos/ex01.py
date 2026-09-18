d08a286d28e2d60dc063fba2d5f8f9d04e1899e6
Patrick Araújo <patrick.araujo@students.ic.unicamp.br>
2026-09-18 01:54:22 -0300

    Update ex01.py

--- Aula02/codigos/ex01.py
+++ Aula02/codigos/ex01.py
@@ -1,5 +1,14 @@
+'''
+    Exercício Tutoreado 1:
+    Peça ao usuário a temperatura atual. Verifique se a temperatura
+    está entre 20 e 25 graus.
+'''
+ 
 def main():
-  print("Hello world!")
+    temp = float(input("Digite a temperatura: "))
+    #   verifica = temp >= 20 and temp <= 25
+    verifica = 20 <= temp <=25
+    print(f"A temperatura está agradável? {verifica}")
   
 if __name__ == "__main__":
   main()
